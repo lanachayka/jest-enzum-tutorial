1dfec679511f52718a62531864070e3d1b98de1c
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-04 15:12:02 +0200

    add Boostrap style

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -6,7 +6,7 @@ Congrats.protoTypes = {
 
 export default function Congrats({success}) {
    return success
-       ? <div data-test="component-congrats">
+       ? <div data-test="component-congrats" className="alert alert-success">
           <span data-test="congrats-message">
           Congratulations! You guessed the word!
           </span>
