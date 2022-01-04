3c0e65a9da6200d5398c142006f0d276be2a5770
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-04 11:07:40 +0200

    add click-counter tests

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -2,7 +2,19 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+   const [count, setCount] = useState(0);
+   const [error, setError] = useState('');
+   const decrement = () => {
+    if(count === 0) {
+        setError('Counter can not go bellow zero');
+    } else {
+        setCount(count - 1);
+    }
+  }
+  const increment = () => {
+       setCount(count + 1);
+       setError('');
+  }
   return (
       <div data-test="component-app" className="App">
         <h1 data-test="counter-display">
@@ -11,9 +23,15 @@ function App() {
         </h1>
         <button
             data-test="increment-button"
-            onClick={() => setCount(count + 1)}>
+            onClick={increment}>
             Increment counter
         </button>
+        <button
+            data-test="decrement-button"
+            onClick={decrement}>
+            Decrement counter
+        </button>
+          <h1 data-test="error-message">{error.length > 0 && error}</h1>
       </div>
   );
 }
--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -2,27 +2,68 @@ import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme, {shallow} from 'enzyme';
 import App from './App';
 
+// set up enzyme`s react adapter
 Enzyme.configure({adapter: new EnzymeAdapter()});
 
-test('renders without error', () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(1);
-});
+//Factory function to crate a ShallowWrapper for the App component
+const setup = () => shallow(<App />);
 
-test('renders increment button', () => {
+//Factory function to find by data-test attribute
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
-});
+describe('App component testing', () => {
+  const wrapper = setup();
+  test('renders without error', () => {
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.length).toBe(1);
+  });
 
-test('renders counter display', () => {
+  test('renders increment button', () => {
+    const button = findByTestAttr(wrapper, 'increment-button');
+    expect(button.length).toBe(1);
+  });
 
-});
+  test('renders counter display', () => {
+    const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+    expect(counterDisplay.length).toBe(1);
+  });
 
-test('counter display starts at 0', () => {
+  test('counter display starts at 0', () => {
+    const count = findByTestAttr(wrapper, 'count').text();
+    expect(count).toBe("0");
+  });
 
-});
+  test('clicking button increments counter display', () => {
+    const button = findByTestAttr(wrapper, 'increment-button');
+    button.simulate('click');
+    const count = findByTestAttr(wrapper, 'count').text();
+    expect(count).toBe('1');
+  });
 
-test('clicking button increments counter display', () => {
+  test('renders decrement button', () => {
+    const button = findByTestAttr(wrapper, 'decrement-button');
+    expect(button.length).toBe(1);
+  });
 
-});
+  test('error massage started with empty string', () => {
+    const error = findByTestAttr(wrapper, 'error-message').text();
+    expect(error.length).toBe(0);
+  });
+
+  test('clicking button decrements with 0 trow an error', () => {
+    const wrapper = setup();
+    const button = findByTestAttr(wrapper, 'decrement-button');
+    button.simulate('click');
+    const error = findByTestAttr(wrapper, 'error-message').text();
+    expect(error).toBe('Counter can not go bellow zero');
+  });
+
+  test('clicking button decrements counter display', () => {
+    const button = findByTestAttr(wrapper, 'decrement-button');
+    button.simulate('click');
+    const count = findByTestAttr(wrapper, 'count').text();
+    expect(count).toBe('0');
+  });
+
+})
 
