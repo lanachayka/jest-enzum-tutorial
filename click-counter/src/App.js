7af797ef283376f1a580524c0c513cc73266dc76
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-04 09:35:10 +0200

    create react app click-counter

---
+++ click-counter/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+  return (
+      <div data-test="component-app" className="App">
+        <h1 data-test="counter-display">
+            The counter is currently&nbsp;
+            <span data-test="count">{count}</span>
+        </h1>
+        <button
+            data-test="increment-button"
+            onClick={() => setCount(count + 1)}>
+            Increment counter
+        </button>
+      </div>
+  );
+}
+
+export default App;
--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Enzyme, {shallow} from 'enzyme';
+
+Enzyme.configure({adapter: new EnzymeAdapter});
+
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders non-empty component without crashing with enzyme', () => {
+   const wrapper = shallow(<App />);
+   // console.log(wrapper.debug()); show HTML
+   expect(wrapper.exists()).toBeTruthy();
+});
+
+
