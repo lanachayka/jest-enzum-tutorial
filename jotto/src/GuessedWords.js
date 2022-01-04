af0430370a4b5bb1987c0a598a4e42821ac1069b
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-04 15:00:18 +0200

    add tests for GuessedWords.js if there are words guessed

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -13,10 +13,28 @@ export default function GuessedWords({ guessedWords }){
         contents = (
             <span data-test="guess-instructions">Try to guess the secret word</span>
         )
+    } else {
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {guessedWords.map((word, index) => (
+                        <tr data-test="guessed-word" key={index}>
+                            <td>{word.guessedWord}</td>
+                            <td>{word.letterMatchCount}</td>
+                        </tr>)
+                    )}
+                    </tbody>
+                </table>
+            </div>
+        )
     }
-    return (
-        <div data-test="guessed-words-component">
-            {contents}
-        </div>
-    )
+    return <div data-test="guessed-words-component">{contents}</div>
 }
--- jotto/src/GuessedWords.test.js
+++ jotto/src/GuessedWords.test.js
@@ -33,5 +33,25 @@ describe('if there are no words guessed', () => {
 });
 
 describe('if there are words guessed', () => {
-
+    const guessedWords = [
+        {guessedWord: 'train', letterMatchCount: 3},
+        {guessedWord: 'agile', letterMatchCount: 1},
+        {guessedWord: 'party', letterMatchCount: 5}
+    ]
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({guessedWords});
+    });
+    test('renders without errors', () => {
+        const component = findByTestAttr(wrapper, 'guessed-words-component');
+        expect(component.length).toBe(1);
+    });
+    test('renders guessed words section', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+        expect(guessedWordsNode.length).toBe(1);
+    });
+    test('correct number of guessed words', () => {
+        const guessedWordsNode = findByTestAttr(wrapper, 'guessed-word');
+        expect(guessedWordsNode.length).toBe(guessedWords.length);
+    });
 });
