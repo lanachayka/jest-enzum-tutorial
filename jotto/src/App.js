c695c3891c61958bf3e7ab04d86d80e12b4a659e
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-05 13:45:30 +0200

    setup redux

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Congrats from "./Congrats";
 import GuessedWords from "./GuessedWords";
 import Input from "./Input";
+import {useEffect} from "react";
+import {getSecretWord} from "./actions";
 
 function App() {
-  return (
+
+  useEffect(() => {
+      getSecretWord();
+  }, []);
+
+    return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={false}/>
       <Input success={false} secretWord={"party"}/>
-      <GuessedWords guessedWords={[{guessedWord: 'train', letterMatchCount: 3}]} />
+      <GuessedWords guessedWords={[]} />
     </div>
   );
 }
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,11 +1,31 @@
 import App from "./App";
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 import {findByTestAttr} from "../test/testUtils";
 
-const setup = () => shallow(<App />);
+//activate global mock
+jest.mock('./actions');
+import {getSecretWord as mockGetSecretWord} from "./actions";
+
+const setup = () => mount(<App />);
 
 test('renders without error', () => {
     const wrapper = setup();
     const appComponent = findByTestAttr(wrapper, 'component-app');
     expect(appComponent).toHaveLength(1);
+});
+
+describe('get secret word', () => {
+    beforeEach(() => {
+       mockGetSecretWord.mockClear();
+    });
+    test('getSecretWord on app mount', () => {
+        const wrapper = setup();
+        expect(mockGetSecretWord).toBeCalledTimes(1);
+    });
+    test('getSecretWord does not run on app update', () => {
+        const wrapper = setup();
+        mockGetSecretWord.mockClear();
+        wrapper.setProps();
+        expect(mockGetSecretWord).toBeCalledTimes(0);
+    });
 });
