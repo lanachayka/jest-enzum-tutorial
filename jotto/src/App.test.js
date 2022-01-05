cbc73fce6c5257867cd40b55e0d43e5590aae382
lanachayka <ruslana.chaikivska@gmail.com>
2022-01-05 09:12:00 +0200

    add base test for App component

---
+++ jotto/src/App.test.js
@@ -0,0 +1,11 @@
+import App from "./App";
+import {shallow} from "enzyme";
+import {findByTestAttr} from "../test/testUtils";
+
+const setup = () => shallow(<App />);
+
+test('renders without error', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent).toHaveLength(1);
+});
